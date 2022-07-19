c19985c48a1516538678e8f24b37aa4b8564f7e4
琬姗 曹 <634068240@qq.com>
2022-07-19 23:10:59 +0800

    安装并配置axios

--- day9/code-users/src/main.js
+++ day9/code-users/src/main.js
@@ -1,9 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+// 1、导入axios
+import axios from 'axios'
 
 Vue.config.productionTip = false
-
+// 2、配置请求根路径
+axios.defaults.baseURL = 'https://www.escook.cn'
+// 3、通过Vue构造函数的原型对象，全局配置axios
+Vue.prototype.$http = axios
 new Vue({
   router,
   render: h => h(App),
