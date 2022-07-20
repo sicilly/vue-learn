f939f6237fc191dbd427af9f6760a935fcb5ea2a
琬姗 曹 <634068240@qq.com>
2022-07-20 11:17:17 +0800

    table组件

--- day9/code-users/src/components/UserList.vue
+++ day9/code-users/src/components/UserList.vue
@@ -1,12 +1,24 @@
 <template>
-  <div>UserList组件</div>
+   <!-- 用户的表格 :data绑定数据源 -->
+    <el-table :data="userList" stripe border>
+        <!-- 这是索引列 -->
+        <el-table-column type="index" label="#"></el-table-column>
+        <!-- 渲染姓名、年龄、头衔、创建时间列 -->
+        <el-table-column label="姓名" prop="name"></el-table-column>
+        <el-table-column label="年龄" prop="age"></el-table-column>
+        <el-table-column label="头衔" prop="position"></el-table-column>
+        <el-table-column label="创建时间" prop="addtime"></el-table-column>
+    </el-table>
 </template>
 
 <script>
 export default {
     name:'UserList',
     data(){
-        userList:[]
+        // 返回一个对象
+        return{
+            userList:[]
+        }
     },
     created(){
         this.getUserList()
